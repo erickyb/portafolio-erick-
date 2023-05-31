ea8f54dc12ec8644386f6d4be5e900733293a986
erickyb <richardyb1985@gmail.com>
2023-05-31 00:03:58 -0500

    mi portafolio

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function () {
+  let navbar = document.getElementById("navbar");
+
+  if (window.pageYOffset > 30) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+});
+
+
+
